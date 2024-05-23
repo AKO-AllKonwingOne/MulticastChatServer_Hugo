3add6e850302f9003cfbbb887c6b947997b02c40
AKO-AllKonwingOne <hugo.lindgren.svensson@gmail.com>
2024-05-23 11:03:21 +0200

    la till portnummer 50000

--- MulticastChatClient_Hugo/src/Main.java
+++ MulticastChatClient_Hugo/src/Main.java
@@ -7,7 +7,7 @@ public class Main {
             throws Exception {
 
         // Default port number we are going to use
-        int portnumber = 5000;
+        int portnumber = 50000;
         if (args.length > 1) {
             portnumber = Integer.parseInt(args[0]);
         }
